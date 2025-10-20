c520a8e9abf3010fdf98d9c2fa708bda2b636fb0
briannanah-ux <briannanah@gmail.com>
2025-10-20 11:25:01 +0100

    Format LockerState struct for better readability

--- parcel-counter-management-system/firmware/include/helpers.h
+++ parcel-counter-management-system/firmware/include/helpers.h
@@ -1,7 +1,16 @@
 #ifndef HELPERS_H
 #define HELPERS_H
+
 #include <Arduino.h>
-struct LockerState { bool occupied; int userId; unsigned long startAt; unsigned long allowedMs; };
+
+struct LockerState {
+  bool occupied;
+  int userId;
+  unsigned long startAt;
+  unsigned long allowedMs;
+};
+
 String escapeJsonString(const String &s);
 String createStatusJson(int lockerIndex, const LockerState &s);
+
 #endif
